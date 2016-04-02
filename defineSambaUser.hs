4b36b522a459a878f44ef8f210bdf3db3b5ee616
root <root@server.mwdesenvolvimento.com.br>
2016-04-02 16:52:07 -0300

    Felipe

--- defineSambaUser.hs
+++ defineSambaUser.hs
@@ -18,10 +18,11 @@ main = do
     , "----------------------------------------------------------------"
     , "To use this program, just enter the names you want to add and the"
     , "whole process of adding a new user to the system and then to Samba"
-    , "will be done to you. Also the passwords will be generated "
+    , "will be done for you. Also the passwords will be generated "
     , "automatically"
     , ""
     , "*Note: remember to restart Samba after changes"
+    , "*Note: remember to add permision to a newSambaUser.bash"
     , "################################################################" ]
   createUser
 
